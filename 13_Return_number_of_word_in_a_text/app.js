b5cf46b34c627b1c191d34f586848f323c3bd043
Rahul Tyagi <rahul204tyagi1234@gmail.com>
2023-06-16 16:34:28 +0530

    data added

---
+++ 13_Return_number_of_word_in_a_text/app.js
@@ -0,0 +1,15 @@
+// //...Create a function that will  return the number of words in a text............................
+// //.....it is the funcation.........................................//.........ar................
+
+function WCount(str) {
+    var totalso = 0;
+    for (var i = 0; i < WCount.length; i++)
+      if (str(i) === " ") { // check the condition if a space is found in str
+        totalso  += 1; // add 1 to total so far
+    }
+  }
+  
+  document.write(WCount("Hii Rahul how are you i don`t like her")); 
+  function WCount(str) { 
+    return str.split(" ").length;
+  }
---
+++ 2_Calculate_the_sum/app.js
@@ -0,0 +1,13 @@
+///...Calculate the sum of numbers with in an array?.............................ar..........
+
+let ar =[4,3,5,7,6,4,1];
+let sum =0;//stor the all number of sum......
+//loop will be start from 0 and last length of Array.......
+for (var i =0;i <ar.length;i++){
+    sum += ar[i];
+}
+document.write(`<h1>All total number- ${sum}</h1>`);
+
+
+
+
